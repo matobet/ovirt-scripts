a0157cd1d4ac90f5cbb0c896c92b7fd17518f686
Martin Betak <mbetak@redhat.com>
2014-08-07 12:53:18 +0200

    Fix environment variable setup

--- bootstrap-build-slave.sh
+++ bootstrap-build-slave.sh
@@ -5,7 +5,7 @@ SCRIPTS_DIR=${1-$HOME/ovirt-scripts}
 [ -d ovirt-engine.git ] || git clone git://gerrit.ovirt.org/ovirt-engine --bare
 ln {$SCRIPTS_DIR,ovirt-engine.git/hooks}/post-receive
 
-echo >> .bashrc <<EOF
+cat >> .bashrc <<EOF
 export PREFIX=$HOME/build
 export SCRIPTS_DIR=$SCRIPTS_DIR
 EOF

6ca4bf072949c44977632808f720cf78457104fa
Martin Betak <mbetak@redhat.com>
2014-08-07 13:07:11 +0200

    bootstrap-dev.sh: Add kernel.shmmax configuration

--- bootstrap-dev.sh
+++ bootstrap-dev.sh
@@ -32,9 +32,15 @@ function setup_db() {
   PGCONFIG=/var/lib/pgsql/data/pg_hba.conf
   sed -i "s/\(all\s\+127\.0\.0\.1\/32\s\+\)ident$/\1password/" $PGCONFIG
   sed -i "s/\(all\s\+::1\/128\s\+\)ident$/\1password/" $PGCONFIG
+  cat >> /etc/sysctl.d/ovirt-postgresql.conf <<EOF
+# ovirt-engine configuration
+kernel.shmmax = 68719476736
+EOF
+  /sbin/sysctl -p /etc/sysctl.d/ovirt-postgresql.conf
   service postgresql restart
   su - postgres -c "psql -d template1 -c \"create user engine password 'engine';\""
   su - postgres -c "psql -d template1 -c \"create database engine owner engine template template0 encoding 'UTF8' lc_collate 'en_US.UTF-8' lc_ctype 'en_US.UTF-8';\""
+  chkconfig postgresql on
 }
 
 install_dependencies

33e0438bce28ed9ad77d7c95d89f04a0dc7e50e6
Martin Betak <mbetak@redhat.com>
2014-08-07 15:24:17 +0200

    build.sh: fix missing prefix

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 
 make clean
-make install-dev DEV_BUILD_GWT_DRAFT=1 DEV_EXTRA_BUILD_FLAGS_GWT_DEFAULTS=" -Dgwt.cssResourceStyle=pretty"
+make install-dev PREFIX=$PREFIX DEV_BUILD_GWT_DRAFT=1 DEV_EXTRA_BUILD_FLAGS_GWT_DEFAULTS=" -Dgwt.cssResourceStyle=pretty"

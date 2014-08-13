3a07811c3b0862577c397fc077ffd55c72abf9c5
Martin Betak <mbetak@redhat.com>
2014-08-13 14:00:14 +0200

    bootstrap-build-slave: create symbolic link for post-receive

--- bootstrap-build-slave.sh
+++ bootstrap-build-slave.sh
@@ -3,9 +3,9 @@
 SCRIPTS_DIR=${1-$HOME/ovirt-scripts}
 
 [ -d ovirt-engine.git ] || git clone git://gerrit.ovirt.org/ovirt-engine --bare
-ln {$SCRIPTS_DIR,ovirt-engine.git/hooks}/post-receive
+ln -s {$SCRIPTS_DIR,ovirt-engine.git/hooks}/post-receive
 
-cat >> .bashrc <<EOF
+cat >> $HOME/.bashrc <<EOF
 export PREFIX=$HOME/build
 export SCRIPTS_DIR=$SCRIPTS_DIR
 EOF

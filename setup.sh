12608566b1fc02d35314f7081dafcea7fa33f6d3
Martin Betak <mbetak@redhat.com>
2014-08-19 14:08:22 +0200

    setup: disable SSL

--- setup.sh
+++ setup.sh
@@ -9,4 +9,7 @@ fi
 echo "Using answer file $FILE:"
 cat $FILE
 yes Yes | $PREFIX/bin/engine-setup --config=$FILE &&
-psql -U engine -c "update vdc_options set option_value='a' where option_name='AdminPassword'"
+psql -U engine -c "UPDATE vdc_options set option_value = 'false' where option_name = 'SSLEnabled'" &&
+psql -U engine -c "UPDATE vdc_options set option_value = 'false' where option_name = 'EncryptHostCommunication'" &&
+psql -U engine -c "UPDATE vdc_options set option_value = 'false' where option_name = 'InstallVds'" &&
+psql -U engine -c "update vdc_options set option_value = 'a' where option_name='AdminPassword'"
